73c505433604ce0bda8f736c5c8cd42e2a393a67
Tuomas Nurminen <tuomas.nurminen.tietoevry@ahti.cloud>
2024-02-29 14:51:20 +0200

    3.7 puhelinluettelo backend step7 done

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const requestLogger = require("./requestLogger");
-const personApi = require('./personsApi')
+const personApi = require("./personsApi");
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(requestLogger.requestLogger);
+app.use(morgan('tiny'))
 
 app.post("/api/persons", (request, response) => {
-  return personApi.createPerson(request, response)
+  return personApi.createPerson(request, response);
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  return personApi.getPerson(request, response)
+  return personApi.getPerson(request, response);
 });
 
 app.get("/api/persons", (request, response) => {
-  return personApi.getPersons(request, response)
+  return personApi.getPersons(request, response);
 });
-
 
 app.get("/api/info", (request, response) => {
   return response.send(personApi.info(request, response));
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  return personApi.deletePerson(request, response)
+  return personApi.deletePerson(request, response);
 });
 
 const PORT = 3001;
