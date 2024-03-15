39eea1a419e7989c0abb1f4477c0ea3b223b5eba
Tuomas Nurminen <tuomas.nurminen.tietoevry@ahti.cloud>
2024-03-15 15:15:45 +0200

    4.8 blogilistan testit step1 - välivaihe

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const { blogsRouter } = require('./controllers/blogs')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+app.use(express.json())
 app.use(blogsRouter)
 app.use(cors())
-app.use(express.json())
 
 module.exports = app
--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 const PORT = 3003
-const BLOGLIST_APP_URI = process.env.MONGODB_BLOGLIST_APP_URI
+const BLOGLIST_APP_URI = process.env.NODE_ENV === 'test'
+ ? process.env.MONGODB_TEST_BLOGLIST_APP_URI
+ : process.env.MONGODB_DEV_BLOGLIST_APP_URI
 
 module.exports = {
     BLOGLIST_APP_URI,
